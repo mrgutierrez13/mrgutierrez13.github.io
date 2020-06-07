3613f08c10a40fed2be46f342fdfc6c39da3d331
moises rumi <moises@mail.com>
2020-06-07 01:18:39 -0400

    navegacion

--- cargar_base.js
+++ cargar_base.js
@@ -4,6 +4,21 @@ fetch("cabezera.html")
   })
   .then((data) => {
     document.querySelector(".cabezera").innerHTML = data;
+
+    // Para que la barra de navegacion siga la pantalla
+    window.onscroll = function () {
+      myFunction();
+    };
+
+    function myFunction() {
+      var header = document.getElementById("head");
+      var sticky = header.clientHeight;
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
   });
 fetch("footer.html")
   .then((response) => {
