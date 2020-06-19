4b8e87cbbd28bfcf9d3b52b1b639034b3972abfb
moises rumi <moises@mail.com>
2020-06-18 23:23:34 -0400

    index paralelo

--- scripts/cargar_nueva.js
+++ scripts/cargar_nueva.js
@@ -1,121 +1,24 @@
 ///CARGAR HTML GENERAL
+///<link href="https://fonts.googleapis.com/css2?family=PT+Sans&family=PT+Serif&display=swap" rel="stylesheet">
+
+'use strict'
+
+const tipo_fuente = document.createElement('link');
+
+tipo_fuente.setAttribute('rel', 'stylesheet');
+
+let str = "https://fonts.googleapis.com/css2?family=PT+Sans&family=PT+Serif&display=swap";
+
+tipo_fuente.setAttribute('href', str);
+
+document.querySelector('head').appendChild(tipo_fuente);
+
+
 fetch("../modules/nueva_cabezera.html")
   .then((response) => {
     return response.text();
   })
   .then((data) => {
-
     document.querySelector(".cabezera").innerHTML = data;
-    
-    // Para que la barra de navegacion siga la pantalla
-    window.onscroll = function () {
-      navStick();
-    };
-
-    function navStick() {
-      var header = document.getElementById("head");
-      var navbar = document.getElementById("navbar");
-      var sticky = header.clientHeight;
-      if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky");
-      } else {
-        navbar.classList.remove("sticky");
-      }
-    }
   });
 
-fetch("../modules/footer.html")
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    document.querySelector("footer").innerHTML = data;
-  });
-
-fetch("../modules/icon_bar.html")
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    document.querySelector(".barra-enlaces").innerHTML = data;
-  });
-
-function toggleNav() {
-  var element1 = document.getElementById("naveg");
-  var element2 = document.getElementById("naveg-ext");
-  var element3 = document.getElementById("nav-icon");
-  element1.classList.toggle("open");
-  element2.classList.toggle("open");
-  element3.classList.toggle("open");
-}
-
-///CARGAR AÑADIDOS: NOTICIA, POP-UP, ETC
-
-fetch("../modules/widgets.html")
-  .then((response) => {
-    return response.text();
-  })
-  .then((data) => {
-    document.querySelector(".widgets").innerHTML = data;
-
-    ///PARA CERRAR MODAL///
-    let button = document.getElementById("close");
-    let cerrar = document.getElementById("close-ext");
-    let modal = document.getElementById("modal");
-
-    if (button !== null && cerrar !== null) {
-      button.addEventListener("click", function (event) {
-        event.preventDefault();
-        modal.style.display = "none";
-      });
-      cerrar.addEventListener("click", function (event) {
-        event.preventDefault();
-        modal.style.display = "none";
-      });
-    }
-  })
-  .catch((error) => {
-    console.log("Esta pagina no carga Widgets");
-  });
-
-///MENSAJE POP-UP
-
-var element = document.getElementById("myForm");
-
-var t = setTimeout(openPopUp, 5000);
-function openPopUp() {
-  element = document.getElementById("myForm");
-  if (element !== null) {
-    element.style.visibility = "visible";
-    element.style.opacity = "1";
-    element.style.maxHeight = "60vmax";
-  }
-}
-
-var element2 = document.getElementById("myButton");
-
-var t2 = setTimeout(openPopUp2, 5500);
-function openPopUp2() {
-  element2 = document.getElementById("myButton");
-  if (element2 !== null) {
-    element2.style.visibility = "visible";
-    element2.style.opacity = "1";
-    element2.style.maxHeight = "60vmax";
-  }
-}
-
-function openForm() {
-  document.getElementById("myForm").style.visibility = "visible";
-  document.getElementById("myForm").style.opacity = "1";
-  document.getElementById("myForm").style.maxHeight = "60vmax";
-  document.getElementById("myButton").style.visibility = "hidden";
-  document.getElementById("myButton").style.opacity = "0";
-}
-
-function closeForm() {
-  document.getElementById("myForm").style.visibility = "hidden";
-  document.getElementById("myForm").style.opacity = "0";
-  document.getElementById("myForm").style.maxHeight = "0";
-  document.getElementById("myButton").style.visibility = "visible";
-  document.getElementById("myButton").style.opacity = "1";
-}
